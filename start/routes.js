d94b8e355b03de97d8ffc56a8b81b3c1dd8fd7b0
João Pedro Pagno <pagno.jp@gmail.com>
2019-10-08 11:52:36 -0300

    add middleware

--- start/routes.js
+++ start/routes.js
@@ -22,8 +22,18 @@ Route.put('/users/:id', 'UserController.update').middleware('auth')
 
 Route.resource('/posts', 'PostController')
   .apiOnly()
-  .middleware('auth')
+  .middleware(['auth', 'is:(administrator || moderator)'])
+
+Route.get('/posts', 'PostController.index')
+  .middleware(['auth', 'can:(read_posts || read_private_posts)'])
+
+Route.get('/posts:id', 'PostController.show')
+  .middleware(['auth', 'can:(read_posts || read_private_posts)'])
 
 Route.resource('/permissions', 'PermissionController')
+  .apiOnly()
+  .middleware('auth')
+
+Route.resource('/roles', 'RoleController')
   .apiOnly()
   .middleware('auth')
